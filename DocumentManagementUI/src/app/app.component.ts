9e0e3f4381a705e639324898d7304cca3fb567e2
Mohammed Alghamdi <Mohammedtg208@gmail.com>
2025-08-13 10:34:33 +0300

    fix: Correct styleUrls property in app.component.ts and clean up styles.css

--- DocumentManagementUI/src/app/app.component.ts
+++ DocumentManagementUI/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './home/header/header.component';
-import { CradComponent } from './crad/crad.component';
-import { DUMMY_DATA_CARD } from '../../Dummy_Data/Dummy_Data';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
   imports: [HeaderComponent, CommonModule, RouterOutlet],
   standalone: true
 })
